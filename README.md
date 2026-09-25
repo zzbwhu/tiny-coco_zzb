3ea75fb6dd125e4a4ad976e6844870a705b32efb
zzb <326787884@qq.com>
2026-09-25 12:13:46 +0800

    refine

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Tiny-COCO
 
 A small coco dataset for visulization, fast loading(downloading) and debugging. 
+
+分别生成对coco2017 train 和 val的采样
+train 注释掉24-33行运行
+val 注释掉11-20行运行
--- create_small_dataset.py
+++ create_small_dataset.py
@@ -4,24 +4,38 @@ from pycocotools.coco import COCO
 import shutil
 
 ####################################################################################################
-source_root = "/home/ubuntu/mscoco"
-datadir = os.path.join(source_root, 'annotations')
-json_file = "instances_train2017.json"
+source_root = "/root/autodl-tmp/coco"
+dest_root = "mini_coco"
 
-dest_root = "small_coco"
-new_json_file = "instances_train2017_small.json"
-new_imgs_name = "train_2017_small" 
+# train
+# anno_json_file = "annotations/instances_train2017.json"
+# imgdatadir = "train2017"
+
+# mini_anno_json_file = "annotations/instances_train2017.json"
+# mini_imgs_name = "train2017" 
+
+# #regenerate = True 
+# regenerate = False 
+# num = 64
+# images_list = [391895, 522418, 184613, 318219, 554625, 574769, 60623, 309022, 5802, 222564, 118113, 193271, 224736, 483108, 403013, 374628]
+####################################################################################################
+
+# val
+anno_json_file = "annotations/instances_val2017.json"
+imgdatadir = "val2017"
+
+mini_anno_json_file = "annotations/instances_val2017.json"
+mini_imgs_name = "val2017" 
 
 #regenerate = True 
 regenerate = False 
 num = 64
-images_list = [391895, 522418, 184613, 318219, 554625, 574769, 60623, 309022, 5802, 222564, 118113, 193271, 224736, 483108, 403013, 374628]
-#images_list = [391895]
+images_list = [139, 285, 632, 724, 776, 785]
 ####################################################################################################
 
 
 def generate_annotations(images_list=images_list):
-    with open(os.path.join(datadir, json_file), 'r') as f:  
+    with open(os.path.join(source_root, anno_json_file), 'r') as f:  
         coco = json.load(f)
 
     new_images = []
@@ -41,21 +55,26 @@ def generate_annotations(images_list=images_list):
     coco['images'] = new_images
     coco['annotations'] = new_annotations
 
+    new_dir_path = os.path.join(dest_root, 'annotations')
+    if not os.path.exists(new_dir_path):
+        # shutil.rmtree(new_dir_path)
+        os.mkdir(new_dir_path)
+
     print("begin to save")
-    with open( os.path.join(dest_root, new_json_file), 'w') as ff:
+    with open( os.path.join(dest_root, mini_anno_json_file), 'w') as ff:
         json.dump(coco, ff)
 
 
 def generate_images(images_list=images_list):
-    new_dir_path = os.path.join(dest_root, new_imgs_name)  
+    new_dir_path = os.path.join(dest_root, mini_imgs_name)  
 
-    if os.path.exists(new_dir_path):
-        shutil.rmtree(new_dir_path)
-    os.mkdir(new_dir_path) 
+    if not os.path.exists(new_dir_path):
+        # shutil.rmtree(new_dir_path)
+        os.mkdir(new_dir_path) 
 
     for img in images_list:
         img_name = format(img, "012") + ".jpg"
-        src_file  = os.path.join(source_root, "train2017", img_name)
+        src_file  = os.path.join(source_root, imgdatadir, img_name)
         dest_file = os.path.join(new_dir_path, img_name)
         shutil.copyfile(src_file, dest_file) 
     
@@ -65,11 +84,9 @@ if regenerate:
     images_list = [item['id'] for item in coco['images'][:num]]
     print(images_list)
 
-if os.path.exists(dest_root):
-    shutil.rmtree(dest_root)
-os.mkdir(dest_root)
+if not os.path.exists(dest_root):
+    # shutil.rmtree(dest_root)
+    os.mkdir(dest_root)
 
 generate_annotations(images_list)
 generate_images(images_list)
-
-
